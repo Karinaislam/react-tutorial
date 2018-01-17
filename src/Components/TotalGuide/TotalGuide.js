eb02e0cf86c4bb6f73ed59d6fe19bad034765b07
Karinaislam <badhon12karina@gmail.com>
2018-01-16 19:30:34 -0600

    21th

--- src/Components/TotalGuide/TotalGuide.js
+++ src/Components/TotalGuide/TotalGuide.js
@@ -42,13 +42,13 @@ class TotalGuide extends Component {
                     <p>Tags, Attributes, Tables,Forms, FilePath, Iframe, Media</p>
                     <p className="totalHead"> CSS3</p>
                     <p>Syntax, BoxModel, Advance CSS, CSS animations, Flexbox, Media Queries, CSS Grid layout, CSS class</p>
-                    <p className="totalHead"> JavaScript</p>
+                    <p className="totalHead"> JavaScript............</p>
                     <p >Data Types, Math, Random, Date,Switch, For loop, While loop, Functions, Object Oriented programming, Functional Programming, DOM Manupulation, API, JSON, AJAX, ES2015, Arrow Function, Promise, Templete String</p>
                     <p className="totalHead"> CSS Frameworks</p>
                     <p> Bootstrap 4, Material UI, Material Design Lite</p>
                     <p className="totalHead"> JS Frameworks</p>
                     <p >React, Angular</p>
-                    <p className="totalHead">Miscellaneous.....</p>
+                    <p className="totalHead">Miscellaneous</p>
                     <p >Best Practice, Debugging, Interview Questions, Unit Test, WebPack, Type Script, SCSS, Data URI</p>
                     
                 </div>
